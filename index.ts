ed6ae950848c083085ebac0275b12a0dd8925476
Mathew Kurian <mwkurian@gmail.com>
2020-04-01 10:58:10 -0700

    Housekeep

--- index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.use(express.static("public"));
-
-app.listen(port, () => console.log(`Listening on ${port}`));
